d4463a22fbc25af9fedb1653e4723d666613d302
tingerli <1825272403@qq.com>
2020-11-25 14:28:43 +0800

    删除冗余文件

--- src/fetch/user.js
+++ src/fetch/user.js
@@ -1,4 +1,5 @@
 export default {
     user_login: {url: '/user/login', method: 'post'},
-    user_getInfo:  {url: '/admin/user/detail', method: 'post'}
+    user_logout: {url: '/admin/user/cancellation', method: 'post'},
+    user_getInfo:  {url: '/admin/user/detail', method: 'post'},
 }
