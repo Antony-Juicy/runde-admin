8fb3238ecfd10ec0f2cb42231acb4f0e4ba5231d
weijinghong <695153881@qq.com>
2020-12-01 18:07:36 +0800

    update 新增通讯录管理json文件

--- src/fetch/system.js
+++ src/fetch/system.js
@@ -1,5 +1,6 @@
 export default {
     getMenuList: {url: '/admin/menu/list', method: 'post'},
     getMenuTreeList:  {url: '/admin/menu/getallmenutree', method: 'get'},
-    system_getCode: {url: '/admin/generate/serial', method: 'post'}
+    system_getCode: {url: '/admin/generate/serial', method: 'post'},
+    getDeptTreeList: {url: '/admin/dept/tree', method: 'post'}, // 获取通讯录管理组织机构树
 }
--- src/views/system-module/system-config/addressbook-management/index.vue
+++ src/views/system-module/system-config/addressbook-management/index.vue
@@ -1,96 +1,31 @@
 <template>
-  <div class="container">
+  <div class="addressbook_container">
     <div class="center_l">
-      <rd-tree :data="dataTree" :defaultProps="defaultProps" @node-click="handleNodeClick">
+      <rd-tree :data="treeData" :defaultProps="defaultProps" @nodeClick="handleNodeClick">
     </rd-tree>
     </div>
     <div class="center_r">
       <div class="center_r_top">
-        <!-- <el-form id="searchBox" :model="formData" ref="formRef" :inline="true">
-          <el-form-item
-            v-for="(item, index) in formOptions"
-            :key="newKeys[index]"
-            :prop="item.prop"
-            :label="item.label ? item.label + '：' : ''"
-            :rules="item.rules">
-            <formItem v-model="formData[item.prop]" :itemOptions="item" />
-          </el-form-item>
-        </el-form> -->
-        <!-- <el-form id="searchBox" :model="formData" ref="formRef" :inline="true"> -->
-
-          <el-input placeholder="请输入姓名" v-model="formData.name" clearable style="width: 20%"></el-input>
-          <el-input placeholder="请输入手机号" v-model="formData.phone" clearable style="width: 20%"></el-input>
-          <el-input placeholder="请输入职位" v-model="formData.zhiwei" clearable style="width: 20%"></el-input>
-          <el-input placeholder="请输入ID" v-model="formData.idcar" clearable style="width: 20%"></el-input>
-        <!-- </el-form> -->
-        <el-button type="primary">搜索</el-button>
-        <el-button type="default" @click="onReset">重置</el-button>
-        <!-- <search-form :formOptions = "formOptions" @onSearch = onSearch></search-form> -->
+        <search-form :formOptions = "formOptions" @onSearch = onSearch></search-form>
       </div>
       <div class="center_r_bottom">
-        <p style="margin: 0;">医学1战队</p>
-        <el-table
-          :data="tableData"
-          border
-          height="500"
-          style="width: 100%;margin-top:10px;">
-          <el-table-column
-            prop=""
-            label="头像"
-            width="80">
-            <template slot-scope="scope">
-              <img :src="scope.row.src" style="width:50px;height:50px;" alt="">
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="姓名"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="date"
-            label="手机"
-            width="150">
-          </el-table-column>
-          <el-table-column
-            prop="province"
-            label="部门"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="city"
-            label="职位"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="角色"
-            width="">
-          </el-table-column>
-          <el-table-column
-            prop="zip"
-            label="状态"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            label="操作"
-            width="120">
-            <template slot-scope="scope">
-              <el-button
-                @click.native.prevent="editRow(scope.$index,scope.row.id, tableData)"
-                type="text"
-                size="small">
-                编辑
-              </el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-        <pagination
-          :total="pageConfig.totalCount"
-          :page.sync="pageConfig.pageNum"
-          :limit.sync="pageConfig.pageSize"
-          @pagination="pageChange"
-        />
+        <p style="font-weight: 700;margin-bottom: 20px;">医学1战队</p>
+        <rd-table
+          :tableData="tableData"
+          :tableKey="tableKey"
+          :loading="loading"
+          :fixedTwoRow="fixedTwoRow"
+          :pageConfig="pageConfig"
+          @select="handleSelect"
+          @pageChange="pageChange"
+        >
+          <template slot="src" slot-scope="scope">
+            <img :src="scope.row.src" style="width:56px;height:56px;" alt="">
+          </template>
+          <template slot="edit" slot-scope="scope">
+            <el-button @click="editRow(scope.$index,scope.row.id,scope.row)" type="text" size="small">编辑</el-button>
+          </template>
+        </rd-table>
       </div>
       <el-drawer
         title=""
@@ -172,6 +107,7 @@ import RdTree from '@/components/RdTree';
 import searchForm from '@/components/Searchform';
 import Pagination from "@/components/Pagination";
 import DescriptionList from "./descriptionList"; 
+ import axios from 'axios';
 export default {
   inject: ["reload"],
   components: {
@@ -183,287 +119,51 @@ export default {
   data () {
     return {
       input: '',
-      // formOptions: [
-      //   {
-      //     prop: 'username',
-      //     element: 'el-input',
-      //     initValue: '',
-      //     placeholder: '请输入姓名',
-      //   },
-      //   {
-      //     prop: 'phone',
-      //     element: 'el-input',
-      //     initValue: '',
-      //     placeholder: '请输入手机号',
-      //   },
-      //   {
-      //     prop: 'zhiwei',
-      //     element: 'el-input',
-      //     initValue: '',
-      //     placeholder: '请输入职位',
-      //   },
-      //   {
-      //     prop: 'idcard',
-      //     element: 'el-input',
-      //     initValue: '',
-      //     placeholder: '请输入ID',
-      //   },
-      // ],
+      formOptions: [
+        {
+          prop: 'username',
+          element: 'el-input',
+          initValue: '',
+          placeholder: '请输入姓名',
+        },
+        {
+          prop: 'phone',
+          element: 'el-input',
+          initValue: '',
+          placeholder: '请输入手机号',
+        },
+        {
+          prop: 'zhiwei',
+          element: 'el-input',
+          initValue: '',
+          placeholder: '请输入职位',
+        },
+        {
+          prop: 'idcard',
+          element: 'el-input',
+          initValue: '',
+          placeholder: '请输入ID',
+        },
+      ],
       formData: {
         name: '',
         phone: '',
         zhiwei: '',
         idcar: ''
       },
-      tableData: [
-        {
-          id: 2,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '战队长',
-          address: '省校长、分校长...',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },{
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
-        {
-          id: 1,
-          src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2763523135,381182796&fm=26&gp=0.jpg',
-          date: '13800138000',
-          name: '飞翔的荷兰人',
-          province: '医学4战队',
-          city: '营销专员',
-          address: '省校长、分校长',
-          zip: '在职'
-        },
+      tableData: [],
+      tableKey: [
+        { name: '头像',value: 'src',operate: true,width: 80 },
+        { name: '姓名',value: 'name' },
+        { name: '手机',value: 'phone' },
+        { name: '部门',value: 'dept' },
+        { name: '职位',value: 'position' },
+        { name: '角色',value: 'roles' },
+        { name: '状态',value: 'status' },
+        { name: '编辑',value: 'edit',operate: true,width: 100 },
       ],
-      dataTree: [
-        {
-          label: "经营管理中心",
-          children: [
-            {
-              label: "广西校区",
-              children: [
-                {
-                  label: "三级 1-1-1",
-                  children: [
-                    {
-                      label: "四级级 1-1-1",
-                      children: [
-                        {
-                          label: "五级级 1-1-1",
-                        },
-                      ],
-                    },
-                  ],
-                }
-              ],
-            },
-            {
-              label: "山东校区",
-              children: [
-                {
-                  label: "三级 1-1-1",
-                  children: [
-                    {
-                      label: "四级级 1-1-1",
-                      children: [
-                        {
-                          label: "五级级 1-1-1",
-                        },
-                      ],
-                    },
-                  ],
-                }
-              ],
-            },
-            {
-              label: "广东校区",
-              children: [
-                {
-                  label: "广州分校",
-                  children: [
-                    {
-                      label: "四级级 1-1-1",
-                      children: [
-                        {
-                          label: "五级级 1-1-1",
-                        },
-                      ],
-                    },
-                  ],
-                },
-                {
-                  label: "佛山分校",
-                  children: [
-                    {
-                      label: "四级级 1-1-1",
-                      children: [
-                        {
-                          label: "五级级 1-1-1",
-                        },
-                      ],
-                    },
-                  ],
-                },
-                {
-                  label: "茂名分校",
-                  children: [
-                    {
-                      label: "医学1战队",
-                      children: [
-                        
-                      ],
-                    },
-                    {
-                      label: "医学4战队",
-                      children: [
-                        
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: "产品开发中心",
-          children: [
-            {
-              label: "二级 2-1",
-              children: [
-                {
-                  label: "三级 2-1-1",
-                },
-              ],
-            },
-            {
-              label: "二级 2-2",
-              children: [
-                {
-                  label: "三级 2-2-1",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: "总裁办",
-          children: [
-            {
-              label: "二级 2-1",
-              children: [],
-            }
-          ],
-        },
-        {
-          label: "药学事业群",
-          children: [
-            {
-              label: "二级 2-1",
-              children: [],
-            }
-          ],
-        },
-        {
-          label: "医学事业群",
-          children: [
-            {
-              label: "二级 2-1",
-              children: [],
-            }
-          ],
-        },
-        {
-          label: "学历事业群",
-          children: [
-            {
-              label: "二级 2-1",
-              children: [],
-            }
-          ],
-        },
-      ],
+      fixedTwoRow: true,
+      treeData: [],
       defaultProps: {
         children: "children",
         label: "label",
@@ -526,28 +226,56 @@ export default {
       timer: null,
     }
   },
+  mounted() {
+    // this.getTreeData();
+    // this.getTableData();
+    this.getDataTree();
+    this.getDataTable();
+  },
   methods: {
+    // 获取通讯录组织树
+    getTreeData() {
+      this.$fetch('getDeptTreeList').then(res => {
+        console.log(res.data.records,'tree')
+        this.treeData = res.data.records;
+      })
+    },
+    // 获取本地通讯录组织树
+    async getDataTree() {
+      const { data } = await axios.get('/json/depttreedata.json')
+      this.treeData = data
+    },
+    // 获取本地组织表格数据
+    async getDataTable() {
+      const { data } = await axios.get('/json/depttabledata.json')
+      this.tableData = data
+      console.log(data,456456)
+    },
+    // 获取组织表格数据
+    getTableData() {
+      console.log('表格数据')
+    },
     onSearch(val) {
       console.log(val, 999)
     },
-    onReset() {
-      console.log(9999)
-      this.$refs.formRef.resetFields();
+    handleSelect(rows) {
+      console.log(rows, "rows---");
     },
     editRow(index,id, rows) {
       // rows.splice(index, 1);
-      console.log(index,id, 666)
+      console.log(index,id,rows, 666)
       // this.drawer = true
       this.dialog = true
     },
     pageChange(val) {
-      this.getTableData({
-        currentPage: val.page,
-        showCount: val.limit,
-      });
+      console.log(666)
+      // this.getTableData({
+      //   currentPage: val.page,
+      //   showCount: val.limit,
+      // });
     },
     handleNodeClick(data) {
-      console.log(data, 9999);
+      console.log(data.id, 9999);
     },
     handleClose(done) {
       if (this.loading) {
@@ -590,98 +318,29 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.container {
-  display: flex;
+.addressbook_container {
+  // display: flex;
   .center_l {
-    width: 20%;
-    // height: 300px;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    .tree-container /deep/ {
-      .el-tree {
-        .el-tree-node__expand-icon.el-icon-caret-right {
-          &::before {
-            content: url(https://rdimg.rundejy.com/web/runde_console/static/new_test_more.png) !important;
-          }
-          &.is-leaf {
-            &::before {
-              content: "" !important;
-            }
-          }
-          &.expanded {
-            transform: none !important;
-            &::before {
-              content: url(https://rdimg.rundejy.com/web/runde_console/static/new_test_less.png) !important;
-            }
-          }
+    float: left;
+    width: 340px;
+    margin-right: 24px;
+    .tree-container {
+        /deep/ .el-tree {
+          padding-top: 24px;
+           height: calc(100vh - 190px);
+           overflow: auto;
+           padding-bottom: 24px;
         }
-      }
-
-      .el-tree > .el-tree-node:after {
-        border-top: none;
-      }
-
-      .el-tree-node {
-        position: relative;
-        padding-left: 16px;
-      }
-
-      .el-tree-node__children {
-        padding-left: 16px;
-      }
-
-      .el-tree-node :last-child:before {
-        height: 38px;
-      }
-
-      .el-tree > .el-tree-node:before {
-        border-left: none;
-      }
-
-      .el-tree-node:before {
-        content: "";
-        left: -4px;
-        position: absolute;
-        right: auto;
-        border-width: 1px;
-      }
-
-      .el-tree-node:after {
-        content: "";
-        left: -4px;
-        position: absolute;
-        right: auto;
-        border-width: 1px;
-      }
-
-      .el-tree-node:before {
-        border-left: 1px solid #e3e3e3;
-        bottom: 0px;
-        height: 100%;
-        top: -26px;
-        width: 1px;
-      }
-
-      .el-tree-node:after {
-        border-top: 1px solid #e3e3e3;
-        height: 20px;
-        top: 12px;
-        width: 24px;
-      }
     }
   }
   .center_r {
-    width: 80%;
-    padding: 20px;
+    overflow: hidden;
+    padding: 20px 20px 0 20px;
     margin-left: 10px;
     background-color: #fff;
-    border: 1px solid #ccc;
-    .center_r_top {
-      // margin-bottom: 20px;
-    }
     .center_r_bottom {
-      height: 600px;
-      padding: 20px 20px 20px 0;
+      // height: 600px;
+      padding: 10px 20px 0 0;
       .pagination-container {
         padding: 20px 16px;
       }
