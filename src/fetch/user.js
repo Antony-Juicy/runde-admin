6a680fd49ba39a8ddc5c4f21c44e69f0f19ab944
weijinghong <695153881@qq.com>
2020-12-04 18:17:03 +0800

    登录页面调整

--- src/fetch/user.js
+++ src/fetch/user.js
@@ -2,5 +2,6 @@ export default {
     user_login: {url: '/user/login', method: 'post'},
     user_logout: {url: '/admin/user/cancellation', method: 'post'},
     user_getInfo:  {url: '/admin/user/detail', method: 'post'},
-    user_getMenuList: {url: '/admin/user/menu/list', method: 'post'}
+    user_getMenuList: {url: '/admin/user/menu/list', method: 'post'},
+    user_getImgcode: {url: '/user/captcha', method: 'get'}
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,53 +6,87 @@
       :rules="loginRules"
       class="login-form"
       auto-complete="on"
+      label-width="auto"
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">后台管理系统 </h3>
+        <h3 class="title">欢迎访问润德大教务平台</h3>
       </div>
+      <div class="tabs-container">
+        <el-tabs v-model="activeName" @tab-click="handleClick" :stretch="true">
+          <el-tab-pane label="账号密码登录" name="first">
+            <el-form-item prop="username">
+              <span class="svg-container">
+                <svg-icon icon-class="user" />
+              </span>
+              <el-input
+                ref="username"
+                v-model="loginForm.username"
+                placeholder="请输入用户名"
+                name="username"
+                type="text"
+                tabindex="1"
+                auto-complete="on"/>
+            </el-form-item>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="请输入用户名"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
+            <el-form-item prop="password">
+              <span class="svg-container">
+                <svg-icon icon-class="password" />
+              </span>
+              <el-input
+                :key="passwordType"
+                ref="password"
+                v-model="loginForm.password"
+                :type="passwordType"
+                placeholder="请输入密码"
+                name="password"
+                tabindex="2"
+                auto-complete="on"
+                @keyup.enter.native="handleLogin"/>
+              <span class="show-pwd" @click="showPwd">
+                <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+              </span>
+            </el-form-item>
 
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="请输入密码"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
+            <el-form-item prop="VerifyCode" style="margin-bottom: 10px;">
+              <span class="svg-container">
+                <!-- <svg-icon icon-class="password" /> -->
+              </span>
+              <el-input
+                ref="VerifyCode"
+                v-model="loginForm.VerifyCode"
+                placeholder="请输入验证码"
+                name="VerifyCode"
+                type="text"
+                tabindex="3"
+                class="verifyCode-input"
+                auto-complete="on"/>
+                <img style="width:40%;height:100%" :src="imgCode" alt="" @click="changeImgcode">
+            </el-form-item>
+            
+            <el-form-item prop="" style="margin: 0;">
+              <el-checkbox v-model="checked">记住密码</el-checkbox>
+              <a href="https://baidu.com" style="float:right;color:#409EFF;">忘记密码</a>
+            </el-form-item>
 
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width:100%;margin-bottom:30px;"
-        @click.native.prevent="handleLogin"
-      >登陆</el-button>
+            <el-button
+              :loading="loading"
+              type="primary"
+              style="width:100%;margin-bottom:4px;"
+              @click.native.prevent="handleLogin">登陆
+            </el-button>
+            <p style="font-size:14px;text-align:center;">
+              建议使用
+              <a href="https://www.google.cn/intl/zh-CN/chrome/" target="_Blank" style="color:#409EFF;">谷歌浏览器</a>
+              访问
+            </p>
+          </el-tab-pane>
+          <el-tab-pane label="钉钉扫码登录" name="second">
+            钉钉二维码
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+      
 
       <div class="tips">
         <!-- <span style="margin-right:20px;">username: admin</span>
@@ -82,10 +116,21 @@ export default {
         callback();
       }
     };
+    const validateVerifyCode = (rule, value, callback) => {
+      if (value.length < 4) {
+        callback(new Error("请输入正确的验证码"));
+      } else {
+        callback();
+      }
+    };
     return {
+      activeName: 'first',
+      checked: false,
+      imgCode: '',
       loginForm: {
         username: "",
-        password: ""
+        password: "",
+        VerifyCode: ""
       },
       loginRules: {
         username: [
@@ -93,6 +138,9 @@ export default {
         ],
         password: [
           { required: true, trigger: "blur", validator: validatePassword }
+        ],
+        VerifyCode: [
+          { required: true, trigger: "blur", validator: validateVerifyCode }
         ]
       },
       loading: false,
@@ -108,7 +156,13 @@ export default {
       immediate: true
     }
   },
+  mounted () {
+    this.getImgcode()
+  },
   methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
     showPwd() {
       if (this.passwordType === "password") {
         this.passwordType = "";
@@ -118,6 +172,14 @@ export default {
       this.$nextTick(() => {
         this.$refs.password.focus();
       });
+    },
+    getImgcode() {
+      this.$fetch('user_getImgcode').then(res => {
+        this.imgCode = res.data.captchaBase64;
+      })
+    },
+    changeImgcode() {
+      this.getImgcode()
     },
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
@@ -144,53 +206,6 @@ export default {
 };
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
 <style lang="scss" scoped>
 $bg: #2d3a4b;
 $dark_gray: #889aa4;
@@ -200,6 +215,9 @@ $light_gray: #eee;
   min-height: 100%;
   width: 100%;
   background-color: $bg;
+  background-image: url('../../assets/bg.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
   overflow: hidden;
 
   .login-form {
@@ -209,6 +227,44 @@ $light_gray: #eee;
     padding: 160px 35px 0;
     margin: 0 auto;
     overflow: hidden;
+  }
+  .tabs-container {
+    // width: 420px;
+    height: 430px;
+    padding: 20px;
+    background-color: #fff;
+    .el-input {
+      display: inline-block;
+      height: 50px;
+      width: 85%;
+
+      input {
+        // background: transparent;
+        // border: 0px;
+        // -webkit-appearance: none;
+        // border-radius: 0px;
+        // padding: 12px 5px 12px 15px;
+        // color: $light_gray;
+        // height: 47px;
+        // caret-color: $cursor;
+
+        &:-webkit-autofill {
+          // box-shadow: 0 0 0px 1000px $bg inset !important;
+          // -webkit-text-fill-color: $cursor !important;
+        }
+      }
+    }
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      margin-bottom: 20px;
+      color: #454545;
+      .verifyCode-input {
+        width: 50%;
+      }
+      .el-checkbox {
+        margin-left: 14px;
+      }
+    }
   }
 
   .tips {
@@ -237,7 +293,7 @@ $light_gray: #eee;
     .title {
       font-size: 26px;
       color: $light_gray;
-      margin: 0px auto 40px auto;
+      margin: 0px auto 20px auto;
       text-align: center;
       font-weight: bold;
     }
@@ -245,7 +301,7 @@ $light_gray: #eee;
 
   .show-pwd {
     position: absolute;
-    right: 10px;
+    right: 40px;
     top: 7px;
     font-size: 16px;
     color: $dark_gray;
