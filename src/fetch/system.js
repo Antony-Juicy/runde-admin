be2d5bc5b219699c4e37fe300e3c2b759674b0db
tingerli <1825272403@qq.com>
2020-12-09 15:14:19 +0800

    update

--- src/fetch/system.js
+++ src/fetch/system.js
@@ -9,6 +9,6 @@ export default {
     role_save: {url:'/admin/sec/role/save', method: 'post'},
     role_edit: {url:'/admin/sec/role/edit', method: 'post'},
     menu_impower: {url:'/admin/sec/menu/impower/menus', method: 'post'},
-    getDeptTreeList: {url: '/admin/staff/campus/list', method: 'post'}, // 获取通讯录管理组织机构树
     getDeptTableList: {url: '/admin/staff/page', method: 'post'}, // 获取通讯录管理部门成员
+    getDeptTreeList: {url: '/admin/staff/campus/list', method: 'post'}, // 获取通讯录管理组织机构树
 }
