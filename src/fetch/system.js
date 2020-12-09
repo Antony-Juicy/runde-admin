3c6fff6fa7c465b21f35446199939b12c4d22cf5
weijinghong <695153881@qq.com>
2020-12-09 15:07:49 +0800

    通讯录管理对接接口

--- src/fetch/system.js
+++ src/fetch/system.js
@@ -2,7 +2,8 @@ export default {
     getMenuList: {url: '/admin/menu/list', method: 'post'},
     getMenuTreeList:  {url: '/admin/menu/getallmenutree', method: 'get'},
     system_getCode: {url: '/admin/generate/serial', method: 'post'},
-    getDeptTreeList: {url: '/admin/dept/tree', method: 'post'}, // 获取通讯录管理组织机构树
+    getDeptTreeList: {url: '/admin/staff/campus/list', method: 'post'}, // 获取通讯录管理组织机构树
+    getDeptTableList: {url: '/admin/staff/page', method: 'post'}, // 获取通讯录管理部门成员
     menu_save: {url:'/admin/menu/save', method: 'post'},
     menu_edit: {url:'/admin/menu/edit', method: 'post'},
     menu_delete: {url:'/admin/menu/del', method: 'post'},
