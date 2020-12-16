2669f0d658c6ccc47f6edc4cd2144f7a452779ad
weijinghong <695153881@qq.com>
2020-12-16 11:25:48 +0800

    crm目录

---
+++ src/views/crm-module/mychance-management/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  inject: ["reload"],
+  name:'mychance-management',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
