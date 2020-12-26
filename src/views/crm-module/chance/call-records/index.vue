72adc89953834f885103ef75f0697b696bc4162c
weijinghong <695153881@qq.com>
2020-12-26 17:46:11 +0800

    导入手机查询新增导入弹窗，通话记录模块：手机外呼，在线外呼总部，在线外呼分校，ai呼叫，一键回呼

---
+++ src/views/crm-module/chance/call-records/index.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="assignchange_container">
+    <div class="w-container">
+      <el-tabs v-model="activeName" @tab-click="handleClick" >
+        <el-tab-pane label="手机外呼" name="second">
+          <search-form :formOptions = "formOptions" :showNum="showNum" @onSearch = onSearch></search-form>
+            <div class="btn-wrapper">
+              <el-radio-group v-model="radio1" @change="handleTabClick">
+                <el-radio-button label="0">全部</el-radio-button>
+                <el-radio-button label="1">呼出</el-radio-button>
+                <el-radio-button label="2">呼入</el-radio-button>
+                <el-radio-button label="3">呼出未接</el-radio-button>
+                <el-radio-button label="4">呼入未接</el-radio-button>
+              </el-radio-group>
+            </div>
+            <rd-table
+              :tableData="tableData"
+              :tableKey="tableKey"
+              :loading="loading"
+              :fixedTwoRow="fixedTwoRow"
+              :pageConfig="pageConfig"
+              @select="handleSelect"
+              @pageChange="pageChange"
+            >
+              <template slot="edit" slot-scope="scope">
+                <el-button @click="handleEdit(scope.row)" type="text" size="small"
+                  >编辑</el-button
+                >
+                <el-divider direction="vertical"></el-divider>
+                <el-button
+                  @click="handleAuthority(scope.row)"
+                  type="text"
+                  size="small"
+                  style="color: #ec5b56"
+                  >配置权限</el-button
+                >
+              </template>
+            </rd-table>
+        </el-tab-pane>
+        <el-tab-pane label="在线外呼（总部）" name="two">
+          <online-incall></online-incall>
+        </el-tab-pane>
+        <el-tab-pane label="在线外呼（分校）" name="three">
+          <online-outcall></online-outcall>
+        </el-tab-pane>
+        <el-tab-pane label="AI呼叫" name="four">
+          <ai-call></ai-call>
+        </el-tab-pane>
+        <el-tab-pane label="一键回呼" name="five">
+          <akey-call></akey-call>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+import searchForm from '@/components/Searchform';
+import onlineIncall from './online-incall';
+import onlineOutcall from './online-outcall';
+import aiCall from './ai-call';
+import akeyCall from './akey-call';
+export default {
+  name:'call-records',
+  components: {
+    searchForm,
+    onlineIncall,
+    onlineOutcall,
+    aiCall,
+    akeyCall
+  },
+  data () {
+    return {
+      showNum: 6,
+      searchForm: {},
+      formOptions: [
+        { prop: 'staff_name', element: 'el-select', initValue: '', placeholder: '招生老师' },
+        { prop: 'contact_name', element: 'el-input', initValue: '', placeholder: '通话人姓名' },
+        { prop: 'campus_name', element: 'el-select', initValue: '', placeholder: '校区名（所属组织）' },
+        { prop: 'contact_phone', element: 'el-input', initValue: '', placeholder: '通话人号码' },
+        { prop: 'data_type', element: 'el-select', initValue: '', placeholder: '通话类型' },
+        { prop: 'createAt', element: 'el-date-picker', initValue: '', placeholder: '', initWidth: true}
+      ],
+      tableData: [],
+      tableKey: [
+        { name: '通话类型',value: 'type' },
+        { name: '通话类型',value: 'data_type' },
+        { name: '通话人名称',value: 'contact_name' },
+        { name: '通话人号码',value: 'contact_phone' },
+        { name: '通话时长(单位秒)',value: 'duration', operate: true,sortable: true },
+        { name: '招生老师',value: 'staff_name' },
+        { name: '校区名',value: 'campus_name' },
+        { name: '接通时间',value: 'start_time' },
+        { name: '结束时间',value: 'end_time' },
+        { name: '录音',value: '10' },
+      ],
+      emptyText: '暂无数据，请选择相应的组织架构',
+      fixedTwoRow: true,
+      pageConfig: {
+        totalCount: 100,
+        pageNum: 1,
+        pageSize: 10,
+      },
+      loading: false,
+
+      //
+      activeName: 'second',
+      radio1: 0,
+    }
+  },
+  methods: {
+    onSearch(val) {
+      this.searchForm = {...val};
+      console.log(val,this.searchForm , 'val---')
+    },
+    handleSelect(rows) {
+      console.log(rows, "rows---");
+    },
+    pageChange(val) {
+      console.log(val,'pagechange')
+    },
+    
+    handleClick(tab, event) {
+      // console.log(tab, event);
+    },
+    // 点击权限组的分类
+    handleTabClick(data){
+      console.log(data,99966)
+      // this.pageConfig.currentPage = 1;
+      // this.getTableData();
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.assignchange_container {
+  .btn-wrapper {
+    margin-bottom: 8px;
+  }
+}
+</style>
---
+++ src/views/crm-module/chance/call-records/online-outcall.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="onlineoutcall-container">
+    <search-form :formOptions = "formOptions" @onSearch = onSearch></search-form>
+    <div class="btn-wrapper">
+      <el-radio-group v-model="radio1" @change="handleTabClick">
+        <el-radio-button label="0">全部</el-radio-button>
+        <el-radio-button label="1">拨号呼入</el-radio-button>
+        <el-radio-button label="2">后台外呼</el-radio-button>
+        <el-radio-button label="3">拨号外呼</el-radio-button>
+        <el-radio-button label="4">点击外呼</el-radio-button>
+      </el-radio-group>
+    </div>
+    <rd-table
+      :tableData="tableData"
+      :tableKey="tableKey"
+      :loading="loading"
+      :fixedTwoRow="fixedTwoRow"
+      :pageConfig="pageConfig"
+      @select="handleSelect"
+      @pageChange="pageChange"
+    >
+    </rd-table>
+  </div>
+</template>
+
+<script>
+import searchForm from '@/components/Searchform';
+export default {
+  components: {
+    searchForm
+  },
+  data () {
+    return {
+      searchForm: {},
+      formOptions: [
+        { prop: 'AgentName', element: 'el-select', initValue: '', placeholder: '招生老师' },
+        { prop: 'CalledNo', element: 'el-input', initValue: '', placeholder: '通话人号码' },
+        { prop: 'campus_name', element: 'el-select', initValue: '', placeholder: '校区名（所属组织）' },
+        { prop: 'End', element: 'el-date-picker', initValue: '', placeholder: '', initWidth: true }
+      ],
+      tableData: [],
+      tableKey: [
+        { name: '通话类型',value: 'callType' },
+        { name: '通话人号码',value: 'userNumber' },
+        { name: '通话状态',value: 'status' },
+        { name: '通话时长(单位秒)',value: 'dealingTime', operate: true,sortable: true },
+        { name: '招生老师',value: 'staffName' },
+        { name: '校区名',value: 'campusName' },
+        { name: '接通时间',value: 'connectTime' },
+        { name: '结束时间',value: 'endTime' },
+        { name: '录音',value: '9' },
+      ],
+      emptyText: '暂无数据，请选择相应的组织架构',
+      fixedTwoRow: true,
+      pageConfig: {
+        totalCount: 100,
+        pageNum: 1,
+        pageSize: 10,
+      },
+      loading: false,
+
+      //
+      radio1: 0,
+    }
+  },
+  methods: {
+    onSearch(val) {
+      this.searchForm = {...val};
+      console.log(val,this.searchForm , 'val---')
+    },
+    handleSelect(rows) {
+      console.log(rows, "rows---");
+    },
+    pageChange(val) {
+      console.log(val,'pagechange')
+    },
+    
+    // 点击权限组的分类
+    handleTabClick(data){
+      console.log(data,99966)
+      // this.pageConfig.currentPage = 1;
+      // this.getTableData();
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.onlineoutcall-container {
+  .btn-wrapper {
+    margin-bottom: 8px;
+  }
+}
+</style>
