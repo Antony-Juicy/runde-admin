4e7dad25f03bdbedd740ce09e90d3bbc719dc06f
weijinghong <695153881@qq.com>
2021-01-29 11:51:26 +0800

    邀请管理，商品订单接口对接

--- src/fetch/weapp/live-details.js
+++ src/fetch/weapp/live-details.js
@@ -5,4 +5,12 @@ export default {
   live_detail_getDetail: {url: '/live/console/live_statistics/get_live_statistics_detail', method: 'post' }, // 数据分析页获取直播统计详情信息
   live_detail_summary: { url: '/live/console/live_statistics/get_summary_data', method: 'post' }, // 获取汇总数据
   live_detail_list: { url: '/live/console/live_statistics/page_list', method: 'post' }, // 分页获取直播统计列表
+  
+  // 邀请统计
+  invite_count_list: { url:'/live/console/live_invite/page_live_invite_data', method: 'post' },
+
+  // 商品订单统计
+  orders_list: { url:'/live/console/order/page_list', method: 'post' },
+  orders_detail: { url:'/live/console/order/get_order_detail', method: 'post' }
+
 }
---
+++ src/views/crm-module/weapp/live-details/invite-count/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="invitecount-container">
+    <search-form :formOptions = "formOptions" :showNum="showNum" @onSearch = onSearch></search-form>
+    <div class="w-container">
+      <rd-table
+        :tableData="tableData"
+        :tableKey="tableKey"
+        :loading="loading"
+        :fixedTwoRow="fixedTwoRow"
+        :pageConfig="pageConfig"
+        @select="handleSelect"
+        @pageChange="pageChange">
+      </rd-table>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:"invite-count",
+  data(){
+    return {
+      showNum: 6,
+      searchForm: {},
+      formOptions: [
+        { prop: 'provincialSchoolName', element: 'el-input', placeholder: '校区名称' },
+        { prop: 'branchSchoolName', element: 'el-input', placeholder: '分校名称' },
+        { prop: 'teacherName', element: 'el-input', placeholder: '推广老师' },
+        { prop: 'liveName', element: 'el-input', placeholder: '直播名称' }
+      ],
+      tableData: [
+        {
+          provincialSchoolName: "广东校区",
+          branchSchoolName: "广州校区",
+          teacherName: "王伯伯",
+          inviteeName: "张三",
+          inviteePhone: 13800138000,
+          liveName: "2021药师终极包过班"
+        }
+      ],
+      tableKey: [
+        { name: '校区',value: 'provincialSchoolName' },
+        { name: '分校',value: 'branchSchoolName' },
+        { name: '推广老师',value: 'teacherName' },
+        { name: '被邀请人',value: 'inviteeName' },
+        { name: '被邀请人手机',value: 'inviteePhone' },
+        { name: '邀请直播名称',value: 'liveName' }
+      ],
+      emptyText: '暂无数据',
+      fixedTwoRow: true,
+      pageConfig: {
+        totalCount: 100,
+        pageNum: 1,
+        pageSize: 10,
+      },
+      loading: false,
+    }
+  },
+  mounted () {
+    this.getTableData()
+  },
+  methods: {
+    onSearch(val) {
+      this.searchForm = {...val};
+      this.pageConfig.pageNum = 1;
+      this.getTableData();
+      console.log(val,this.searchForm , 'val---')
+    },
+    handleSelect(rows) {
+      console.log(rows, "rows---");
+    },
+    getTableData(params) {
+      const loading = this.$loading({
+        lock: true,
+        target: ".el-table",
+      });
+      this.$fetch(
+        "invite_count_list",
+        params || {
+          ...this.pageConfig,
+          ...this.searchForm
+        }
+      ).then((res) => {
+        this.tableData = res.data.records;
+        this.pageConfig.totalCount = res.data.totalCount;
+        setTimeout(() => {
+          loading.close();
+        }, 200);
+      });
+    },
+    pageChange(val) {
+      console.log(val,'pagechange')
+      this.getTableData({
+        pageNum: (val && val.page) || 1,
+        pageSize: (val && val.limit) || 10,
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
