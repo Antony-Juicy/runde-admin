564c801520c3deb408ebdac4fc909716edc7d3b6
weijinghong <695153881@qq.com>
2021-02-19 18:36:39 +0800

    新增商品订单详情页面

--- src/views/crm-module/weapp/live-details/goods-orders/index.vue
+++ src/views/crm-module/weapp/live-details/goods-orders/index.vue
@@ -20,14 +20,124 @@
           <el-button @click="openConfig(scope.row)" type="text" size="small">查看明细</el-button>
         </template>
       </rd-table>
+      <fullDialog v-model="ordersVisible" :title="ordersTitle" @change="ordersVisible = false">
+        <div class="title_drawer">订单详情</div>
+        <el-row style="margin-left: 20px;" :gutter="24">
+          <el-col :span="8">
+            <div class="term">订单id：</div>
+            <div class="detail">{{orderInfo.orderId}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">订单状态：</div>
+            <div class="detail">{{orderInfo.orderStatus | orderStatusFilter}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">下单时间：</div>
+            <div class="detail">{{orderInfo.createAt}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">结算时间：</div>
+            <div class="detail">{{orderInfo.payTime}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">交易单号：</div>
+            <div class="detail">{{orderInfo.payOrderId}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">支付方式：</div>
+            <div class="detail">{{orderInfo.payType}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">直播名称：</div>
+            <div class="detail">{{orderInfo.sourceName}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">推广老师：</div>
+            <div class="detail">{{orderInfo.teacherInfo}}</div>
+          </el-col>
+        </el-row>
+        <div class="title_drawer">买家信息</div>
+        <el-row style="margin-left: 20px;" :gutter="24">
+          <el-col :span="8">
+            <div class="term">买家昵称：</div>
+            <div class="detail">{{buyerInfo.nickName}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">买家手机号：</div>
+            <div class="detail">{{buyerInfo.phone}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">支付时间：</div>
+            <div class="detail">{{buyerInfo.payTime}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">支付金额：</div>
+            <div class="detail">{{buyerInfo.actuallyPaid}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">收货人：</div>
+            <div class="detail">{{buyerInfo.consignee}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">联系电话：</div>
+            <div class="detail">{{buyerInfo.consigneePhone}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">收货地址：</div>
+            <div class="detail">{{buyerInfo.deliveryAddress}}</div>
+          </el-col>
+        </el-row>
+        <div class="title_drawer">商品信息</div>
+        <el-row style="margin-left: 20px;" :gutter="24">
+          <el-col :span="8">
+            <div class="term">商品名称：</div>
+            <div class="detail">{{goodsInfo.goodsName}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">商品id：</div>
+            <div class="detail">{{goodsInfo.goodsId}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">规格：</div>
+            <div class="detail">{{goodsInfo.goodsItems}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">开通班型id：</div>
+            <div class="detail">{{goodsInfo.openClassId}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">开通科目id：</div>
+            <div class="detail">{{goodsInfo.openSubjectId}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">配送内容：</div>
+            <div class="detail">{{goodsInfo.ancillaryGoods}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">优惠：</div>
+            <div class="detail">{{goodsInfo.discountAmount}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">配送费：</div>
+            <div class="detail">{{goodsInfo.freight}}</div>
+          </el-col>
+          <el-col :span="8">
+            <div class="term">小计：</div>
+            <div class="detail">{{goodsInfo.paymentAmount}}</div>
+          </el-col>
+        </el-row>
+      </fullDialog>
     </div>
   </div>
 </template>
 
 <script>
-
+import fullDialog from '@/components/FullDialog';
 export default {
   name:"goods-orders",
+  components: {
+    fullDialog
+  },
   data(){
     return {
       showNum: 6,
@@ -81,6 +191,13 @@ export default {
         pageSize: 10,
       },
       loading: false,
+
+      // 订单详情弹窗
+      ordersVisible: false,
+      ordersTitle: '',
+      buyerInfo: {},
+      goodsInfo: {},
+      orderInfo: {}
     }
   },
   mounted () {
@@ -170,6 +287,22 @@ export default {
       this.pageConfig.pageNum = val.page;
       this.pageConfig.pageSize = val.limit;
       this.getTableData();
+    },
+    openConfig(row) {
+      console.log(row,'row----')
+      this.ordersVisible = true;
+      this.ordersTitle = row.goodsName;
+      this.$fetch(
+        "orders_detail",
+        {
+          orderId: row.orderId
+        }
+      ).then((res) => {
+        console.log(res,'res-----------')
+        this.buyerInfo = res.data.buyerInfo
+        this.goodsInfo = res.data.goodsInfo
+        this.orderInfo = res.data.orderInfo
+      });
     }
   },
   filters: {
@@ -191,6 +324,33 @@ export default {
 
 <style lang="scss" scoped>
 .goodsorders-container {
-  
+  .term {
+    color: #606266;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 22px;
+    padding-bottom: 16px;
+    margin-right: 8px;
+    white-space: nowrap;
+    display: table-cell;
+  }
+  .detail {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 1.5;
+    width: 100%;
+    padding-bottom: 16px;
+    color: #606266;
+    display: table-cell;
+  }
+  .title_drawer {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 1.5;
+    padding: 10px 0;
+    margin-bottom: 20px;
+    color: rgb(55, 163, 255);;
+    background-color: rgb(229, 237, 244);
+  }
 }
 </style>
