262652374b1d618767ac43823ef1b2a45956e956
weijinghong <695153881@qq.com>
2021-03-02 14:37:52 +0800

    地推卡管理抽离

--- src/views/activity-module/project/common/ground-push/index.vue
+++ src/views/activity-module/project/common/ground-push/index.vue
@@ -1,323 +1,51 @@
 <template>
-  <div class="post-manage">
-      <search-form
-      :formOptions="formOptions"
-      :showNum="7"
-      @onSearch="onSearch"
-    ></search-form>
+  <div class="groundpush-container">
     <div class="w-container">
-      <div class="btn-wrapper">
-        <el-button type="primary" size="small" @click="handleAdd"
-          >添加</el-button
-        >
-      </div>
-      <rd-table
-        :tableData="tableData"
-        :tableKey="tableKey"
-        :pageConfig.sync="pageConfig"
-        :tbodyHeight="600"
-        fixedTwoRow
-        @pageChange="pageChange"
-        :emptyText="emptyText"
-      >
-        <template slot="edit" slot-scope="scope">
-          <el-button @click="handleEdit(scope.row)" type="text" size="small"
-            >查阅/编辑</el-button
-          >
-          <el-divider direction="vertical"></el-divider>
-          <el-button
-            @click="handleDelete(scope.row)"
-            type="text"
-            size="small"
-            style="color: #ec5b56"
-            >删除</el-button
-          >
-        </template>
-      </rd-table>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="优惠券领取管理" name="first">
+          <Manage ref="Manage"></Manage>
+        </el-tab-pane>
+        <el-tab-pane label="优惠券领取统计" name="second">
+          <Count ref="Count"></Count>
+        </el-tab-pane>
+      </el-tabs>
     </div>
-    
-    <!-- 添加海报 -->
-    <rd-dialog
-        :title="addStatus?'添加海报':'编辑海报'"
-        :dialogVisible="addVisible"
-        @handleClose="addVisible = false"
-        @submitForm="submitAddForm('dataForm3')"
-      >
-        <RdForm :formOptions="addFormOptions" formLabelWidth="120px" :rules="addRules" ref="dataForm3">
-          <template slot="post">
-            <el-button size="small" type="primary">上传海报</el-button>
-          </template>
-        </RdForm>
-      </rd-dialog>
   </div>
 </template>
 
 <script>
-import RdForm from "@/components/RdForm";
+import Manage from "./manage";
+import Count from "./count";
 export default {
   name:"post-manage",
+  components:{
+    Manage,
+    Count
+  },
   data(){
     return {
-      formOptions: [
-        {
-          prop: "menuName",
-          element: "el-input",
-          placeholder: "商品名称",
-        },
-        {
-          prop: "menuName",
-          element: "el-input",
-          placeholder: "活动名称",
-        },
-        {
-          prop: "menuName",
-          element: "el-input",
-          placeholder: "海报名称",
-        }
-      ],
-      searchForm:{},
-      emptyText:"暂无数据",
-      tableData:[
-         {
-          id: 1,
-          name: "飞翔的荷兰人3",
-          cutdown: 1608897351706,
-          visit: 2,
-          phone: "15692026183",
-        },
-      ],
-      tableKey: [
-        {
-          name: "ID主键",
-          value: "id",
-          fixed: "left",
-          width: 80
-        },
-        {
-          name: "老师名称",
-          value: "staffName",
-        },
-        {
-          name: "商品名称",
-          value: "goodsName",
-        },
-        {
-          name: "活动名称",
-          value: "activityName",
-        },
-        {
-          name: "海报名称",
-          value: "posterName",
-        },
-        {
-          name: "海报图片",
-          value: "posterPic",
-        },
-        {
-          name: "分享文案一",
-          value: "posterCopyFirst",
-        },
-        {
-          name: "分享文案二",
-          value: "posterCopySecond",
-        },
-        {
-          name: "分享文案三",
-          value: "posterCopyThird",
-        },
-        {
-          name: "分享文案四",
-          value: "posterCopyFourth",
-        },
-        {
-          name: "分享文案五",
-          value: "posterCopyFifth",
-        },
-        {
-          name: "创建时间",
-          value: "createAt",
-        },
-        {
-          name: "修改时间",
-          value: "updateAt",
-        },
-        {
-          name: "操作",
-          value: "edit",
-          operate: true,
-          width: 140,
-          fixed: "right"
-        },
-      ],
-       pageConfig: {
-        totalCount: 0,
-        currentPage: 1,
-        pageSize: 10,
-      },
-      addVisible: false,
-      addFormOptions: [
-          
-        {
-          prop: "menuName",
-          element: "el-input",
-          placeholder: "请输入海报名称",
-          label: "海报名称"
-        },
-        {
-          prop: "post",
-          element: "el-input",
-          placeholder: "",
-          label: "上传海报",
-          operate: true,
-          initValue: 0
-        },
-        {
-          prop: "roleName",
-          element: "el-select",
-          placeholder: "请选择",
-          label: "所属九块九包邮",
-          options: [
-            {
-              label: "博士",
-              value: "0",
-            },
-            {
-              label: "硕士",
-              value: 1,
-            },
-          ],
-        },
-        {
-          prop: "roleName",
-          element: "el-select",
-          placeholder: "请选择",
-          label: "所属活动",
-          options: [
-            {
-              label: "博士",
-              value: "0",
-            },
-            {
-              label: "硕士",
-              value: 1,
-            },
-          ],
-        },
-        {
-          prop: "menuName3",
-          element: "el-input",
-          placeholder: "请输入",
-          label: "分享分案一",
-          type:"textarea",
-          rows: 2
-        },
-         {
-          prop: "menuName3",
-          element: "el-input",
-          placeholder: "请输入",
-          label: "分享分案二",
-          type:"textarea",
-          rows: 2
-        },
-         {
-          prop: "menuName3",
-          element: "el-input",
-          placeholder: "请输入",
-          label: "分享分案三",
-          type:"textarea",
-          rows: 2
-        },
-         {
-          prop: "menuName3",
-          element: "el-input",
-          placeholder: "请输入",
-          label: "分享分案四",
-          type:"textarea",
-          rows: 2
-        },
-           {
-          prop: "menuName3",
-          element: "el-input",
-          placeholder: "请输入",
-          label: "分享分案五",
-          type:"textarea",
-          rows: 2
-        }
-      ],
-      addRules:{
-        updateReason: [
-          { required: true, message: "请输入修改事由", trigger: "blur" },
-        ]
-      },
-      addStatus: true
+      activeName: "first",
+      tabIndex: "0",
     }
   },
-  components:{
-    RdForm
-  },
-   methods: {
-     onSearch(val){
-       this.searchForm = {
-        ...val
-      };
-      console.log(val,this.searchForm , 'val---')
-      this.getTableData();
-     },
-     getTableData(){
-
-     },
-     pageChange(val) {
-      console.log(val,'pagechange')
-      this.pageConfig.currentPage = val.page;
-      this.pageConfig.showCount = val.limit;
-      this.getTableData();
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab.index, "click");
+      // this.tabIndex = tab.index;
+      if(tab.index == 0){
+        this.$refs.Manage.getTableData();
+      }else if(tab.index == 1){
+        this.$refs.Count.getTableData();
+      }
     },
-    handleAdd(){
-      this.addVisible = true;
-    },
-    submitAddForm(formName){
-      this.$refs[formName].validate((valid, formData) => {
-        if(valid){
-          console.log(formData, "提交");
-        }
-          
-      });
-    },
-    handleEdit(data){
-      this.addStatus = false;
-      this.addVisible = true;
-    },
-    handleDelete(row) {
-      let info = '海报';
-      this.$confirm(`此操作将删除此${info}, 是否继续?`, "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(async () => {
-          const res = await this.$fetch("projectType_delete", {
-            typeId: row.typeId,
-            loginUserId,
-          }).then((res) => {
-            if (res) {
-              this.$message({
-                message: "删除成功",
-                type: "success",
-              });
-              setTimeout(() => {
-                this.getTableData();
-              }, 50);
-            }
-          });
-        })
-        .catch(() => {});
-    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.post-manage {
-
+.groundpush-container {
+  .top-total {
+    // padding-bottom: 4px;
+  }
 }
 </style>
