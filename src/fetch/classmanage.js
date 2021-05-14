5f061641adb3c147840a4a19a20e6314528cce36
tingerli <1825272403@qq.com>
2021-05-14 11:30:19 +0800

    update

--- src/fetch/classmanage.js
+++ src/fetch/classmanage.js
@@ -1,50 +1,47 @@
-export default {//项目内容管理
-    courseProductContent_listJsp: {url: '/admin/courseProductContent/listJsp', method: 'get'}, //获取班型内容列表
-    getProductList: {url: '/admin/courseProductContent/getProductList', method: 'get'}, //获取班型内容列表
-
+export default {
     // 班型内容管理
-    courseProductContent_listJsp: {url: '/admin/courseProductContent/listJsp', method: 'get'},
-    courseProductContent_save: {url: '/admin/courseProductContent/save', method: 'post'},
-    courseProductContent_edit: {url: '/admin/courseProductContent/edit', method: 'put'},
-    courseProductContent_delete: {url: '/admin/courseProductContent/delete', method: 'delete'},
-    courseProductContent_stageGroupList: {url: '/admin/courseProductContent/courseClasstypeStageGroup', method: 'get'},//根据项目id获取班型阶段
-    courseProductContent_getProductList: {url: '/admin/courseProductContent/getProductList', method: 'get'}, //获取项目列表
-    courseProductContent_udpateStatus: {url: '/admin/courseProductContent/udpateStatus', method: 'put'}, //修改状态
-    courseProductContent_goAccountingRulesSelects: {url: '/admin/courseProductContent/goAccountingRulesSelects', method: 'get'}, //核算规则下拉
+    courseProductContent_listJsp: {url: '/teach/courseProductContent/listJsp', method: 'get'},
+    courseProductContent_save: {url: '/teach/courseProductContent/save', method: 'post'},
+    courseProductContent_edit: {url: '/teach/courseProductContent/edit', method: 'put'},
+    courseProductContent_delete: {url: '/teach/courseProductContent/delete', method: 'delete'},
+    courseProductContent_stageGroupList: {url: '/teach/courseProductContent/courseClasstypeStageGroup', method: 'get'},//根据项目id获取班型阶段
+    courseProductContent_getProductList: {url: '/teach/courseProductContent/getProductList', method: 'get'}, //获取项目列表
+    courseProductContent_udpateStatus: {url: '/teach/courseProductContent/udpateStatus', method: 'put'}, //修改状态
+    courseProductContent_goAccountingRulesSelects: {url: '/teach/courseProductContent/goAccountingRulesSelects', method: 'get'}, //核算规则下拉
 
     // 网课编码
-    courseclasstypemiddle_listJsp: {url: '/admin/courseclasstypemiddle/listJsp', method: 'get'},
-    courseclasstypemiddle_goAddWindows: {url: '/admin/courseclasstypemiddle/goAddWindows', method: 'get'},
-    courseclasstypemiddle_save: {url: '/admin/courseclasstypemiddle/save', method: 'post'},
-    courseclasstypemiddle_editJsp: {url: '/admin/courseclasstypemiddle/editJsp', method: 'post'},
-    courseclasstypemiddle_deleteJsp: {url: '/admin/courseclasstypemiddle/deleteJsp', method: 'post'},
-    courseclasstypemiddle_synCourseLiveCode: {url: '/admin/courseclasstypemiddle/synCourseLiveCode', method: 'get'},
-    courseclasstypemiddle_goEdit: {url: '/admin/courseclasstypemiddle/goEdit', method: 'get'},
+    courseclasstypemiddle_listJsp: {url: '/teach/courseclasstypemiddle/listJsp', method: 'get'},
+    courseclasstypemiddle_goAddWindows: {url: '/teach/courseclasstypemiddle/goAddWindows', method: 'get'},
+    courseclasstypemiddle_save: {url: '/teach/courseclasstypemiddle/save', method: 'post'},
+    courseclasstypemiddle_editJsp: {url: '/teach/courseclasstypemiddle/editJsp', method: 'post'},
+    courseclasstypemiddle_deleteJsp: {url: '/teach/courseclasstypemiddle/deleteJsp', method: 'post'},
+    courseclasstypemiddle_synCourseLiveCode: {url: '/teach/courseclasstypemiddle/synCourseLiveCode', method: 'get'},
+    courseclasstypemiddle_goEdit: {url: '/teach/courseclasstypemiddle/goEdit', method: 'get'},
 
     // 直播编码
-    courseclasstypelivecode_listJsp: {url: '/admin/courseclasstypelivecode/listJsp', method: 'get'},
-    courseclasstypelivecode_save: {url: '/admin/courseclasstypelivecode/save', method: 'post'},
-    courseclasstypelivecode_editJsp: {url: '/admin/courseclasstypelivecode/editJsp', method: 'put'},
-    courseclasstypelivecode_deleteJsp: {url: '/admin/courseclasstypelivecode/deleteJsp', method: 'post'},
-    courseclasstypelivecode_goAddWindows: {url: '/admin/courseclasstypelivecode/goAddWindows', method: 'get'},
-    courseclasstypelivecode_goEdit: {url: '/admin/courseclasstypelivecode/goEdit', method: 'get'},
+    courseclasstypelivecode_listJsp: {url: '/teach/courseclasstypelivecode/listJsp', method: 'get'},
+    courseclasstypelivecode_save: {url: '/teach/courseclasstypelivecode/save', method: 'post'},
+    courseclasstypelivecode_editJsp: {url: '/teach/courseclasstypelivecode/editJsp', method: 'put'},
+    courseclasstypelivecode_deleteJsp: {url: '/teach/courseclasstypelivecode/deleteJsp', method: 'post'},
+    courseclasstypelivecode_goAddWindows: {url: '/teach/courseclasstypelivecode/goAddWindows', method: 'get'},
+    courseclasstypelivecode_goEdit: {url: '/teach/courseclasstypelivecode/goEdit', method: 'get'},
 
     // 音频编码
-    courseclasstypefrequencycode_listJsp: {url: '/admin/courseclasstypefrequencycode/listJsp', method: 'get'},
-    courseclasstypefrequencycode_save: {url: '/admin/courseclasstypefrequencycode/save', method: 'post'},
-    courseclasstypefrequencycode_editJsp: {url: '/admin/courseclasstypefrequencycode/editJsp', method: 'post'},
-    courseclasstypefrequencycode_deleteJsp: {url: '/admin/courseclasstypefrequencycode/deleteJsp', method: 'post'},
-    courseclasstypefrequencycode_goAddWindows: {url: '/admin/courseclasstypefrequencycode/goAddWindows', method: 'get'},
-    courseclasstypefrequencycode_goEdit: {url: '/admin/courseclasstypefrequencycode/goEdit', method: 'get'},
+    courseclasstypefrequencycode_listJsp: {url: '/teach/courseclasstypefrequencycode/listJsp', method: 'get'},
+    courseclasstypefrequencycode_save: {url: '/teach/courseclasstypefrequencycode/save', method: 'post'},
+    courseclasstypefrequencycode_editJsp: {url: '/teach/courseclasstypefrequencycode/editJsp', method: 'post'},
+    courseclasstypefrequencycode_deleteJsp: {url: '/teach/courseclasstypefrequencycode/deleteJsp', method: 'post'},
+    courseclasstypefrequencycode_goAddWindows: {url: '/teach/courseclasstypefrequencycode/goAddWindows', method: 'get'},
+    courseclasstypefrequencycode_goEdit: {url: '/teach/courseclasstypefrequencycode/goEdit', method: 'get'},
 
     // 配送图书
-    bookclasstypecoursemiddle_listJsp: {url: '/admin/bookclasstypecoursemiddle/lookListJsp', method: 'get'},
-    bookclasstypecoursemiddle_save: {url: '/admin/bookclasstypecoursemiddle/save', method: 'post'},
-    bookclasstypecoursemiddle_editJsp: {url: '/admin/bookclasstypecoursemiddle/editJsp', method: 'post'},
-    bookclasstypecoursemiddle_deleteJsp: {url: '/admin/bookclasstypecoursemiddle/deleteJsp', method: 'post'},
-    bookclasstypecoursemiddle_goAddWindows: {url: '/admin/bookclasstypecoursemiddle/goAddWindows', method: 'get'},
-    bookclasstypecoursemiddle_goEdit: {url: '/admin/bookclasstypecoursemiddle/goEdit', method: 'get'},
-    bookclasstypecoursemiddle_checkByClassTypeCourseBookId: {url: '/admin/bookclasstypecoursemiddle/checkByClassTypeCourseBookId', method: 'post'},
+    bookclasstypecoursemiddle_listJsp: {url: '/teach/bookclasstypecoursemiddle/lookListJsp', method: 'get'},
+    bookclasstypecoursemiddle_save: {url: '/teach/bookclasstypecoursemiddle/save', method: 'post'},
+    bookclasstypecoursemiddle_editJsp: {url: '/teach/bookclasstypecoursemiddle/editJsp', method: 'post'},
+    bookclasstypecoursemiddle_deleteJsp: {url: '/teach/bookclasstypecoursemiddle/deleteJsp', method: 'post'},
+    bookclasstypecoursemiddle_goAddWindows: {url: '/teach/bookclasstypecoursemiddle/goAddWindows', method: 'get'},
+    bookclasstypecoursemiddle_goEdit: {url: '/teach/bookclasstypecoursemiddle/goEdit', method: 'get'},
+    bookclasstypecoursemiddle_checkByClassTypeCourseBookId: {url: '/teach/bookclasstypecoursemiddle/checkByClassTypeCourseBookId', method: 'post'},
 
     // 班次管理
     courseclass_listJsp: {url: '/teach/courseclass/listJsp', method: 'get'},
