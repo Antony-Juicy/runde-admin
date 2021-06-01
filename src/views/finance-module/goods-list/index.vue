d26ab2995e0a6b08397445e25c3ac19e26b41bbd
PC-20210304YAOE\Administrator <1072686087@qq.com>
2021-06-01 13:50:48 +0800

    根据需求优化课程购买

--- src/views/finance-module/goods-list/index.vue
+++ src/views/finance-module/goods-list/index.vue
@@ -125,8 +125,8 @@
           <el-row>
             <el-col :span="24">
               <el-form-item
-                label="查询学员信息"
-                prop="idCardAndPhone"
+                label="身份证"
+                prop="cardId"
                 :rules="[
                   {
                     required: true,
@@ -140,14 +140,16 @@
                 ]"
               >
                 <el-input
-                  v-model="stuForm.idCardAndPhone"
+                  v-model="stuForm.cardId"
                   style="width: 280px; margin-right: 10px"
                   :maxlength="18"
+                  @blur="handleCheckInfo"
+                  :disabled="disabled"
                   placeholder="请输入学员身份证"
                 ></el-input>
-                <el-button type="primary" @click="handleCheckInfo"
+                <!-- <el-button type="primary" @click="handleCheckInfo"
                   >查询</el-button
-                >
+                > -->
                 <el-button type="primary" @click="handleResetInfo"
                   >重置</el-button
                 >
@@ -196,6 +198,7 @@
                   @change="changeSelect"
                   v-model="stuForm.studentType"
                   style="width: 200px"
+                  :disabled="disabled"
                   placeholder="请选择学员类型"
                 >
                   <el-option
@@ -847,6 +850,7 @@ export default {
   name: "goods-list",
   data() {
     return {
+      disabled:false,
       validator: Common._validatorPhone,
       validator2: Common._isCardNo,
       testData: "",
@@ -941,7 +945,7 @@ export default {
       stuForm: {
         idCardAndPhone: "",
         studentName: "",
-        studentType: "",
+        studentType: "New",
         classBatch: "",
         saleSource: "",
         mediatorName: "",
@@ -1044,6 +1048,7 @@ export default {
       this.$refs.stuForm2.resetFields();
       this.stuForm2.campusName = this.itemData.campusName;
       this.stuForm2.classType = this.itemData.className;
+      this.stuForm.classBatch = this.itemData.classBatch;
       this.$refs.stuForm3.resetFields();
       this.$refs.stuForm4.resetFields();
     },
@@ -1199,45 +1204,115 @@ export default {
     },
     handleResetInfo() {
       //重置
+      this.disabled = false;
       this.$refs.stuForm.resetFields();
       this.$refs.stuForm2.resetFields();
       this.stuForm2.campusName = this.itemData.campusName;
       this.stuForm2.classType = this.itemData.className;
+      this.stuForm.classBatch = this.itemData.classBatch;
+    },
+    open(studentCampusModel) {
+      this.$alert(
+        ` 
+        <h3>经系统查询，该学员为老学员，个人信息如下：</h3>
+        <div>
+        <p><span>姓名：</span> <span>${studentCampusModel.studentName}</span></p> 
+          <p> <span>手机号：</span> <span>${studentCampusModel.phone}</span> </p>
+           <p>  <span>身份证号：</span> <span>${studentCampusModel.cardId}</span> </p>
+        </div>
+        <div style="margin-top:40px;font-size:16px;font-weight:500;">
+        是否自动填充学员信息？
+        </div>
+        `,
+        "学员查询结果",
+        {
+          dangerouslyUseHTMLString: true,
+          confirmButtonText: "是",
+          cancelButtonText: "否",
+          showCancelButton: true,
+          customClass: "elmgs_confirm-box",
+          confirmButtonClass: "confirmButtonClass",
+          cancelButtonClass: "cancelButtonClass",
+        }
+      )
+        .then(() => {
+          //老学员
+          this.$message({
+            type: "success",
+            message: "填充学员信息成功",
+          });
+          this.disabled = true;
+          for (var key in studentCampusModel) {
+            if (this.stuForm[key] != undefined) {
+              this.stuForm[key] = studentCampusModel[key];
+            }
+          }
+          this.stuForm["provincinitValue"] = studentCampusModel["provinc"];
+          this.stuForm["cityinitValue"] = studentCampusModel["city"];
+          this.stuForm["countyinitValue"] = studentCampusModel["county"];
+          console.log("countycountycounty1111", this.stuForm);
+          this.stuForm.studentType = "Old";
+
+          this.$nextTick(() => {//清除校验
+            for (let key in this.stuForm) {
+              if (this.stuForm[key] !== "") {
+                this.$refs.stuForm.clearValidate(`${key}`);
+              }
+            }
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消填充学员信息",
+          });
+          this.stuForm.studentType = "New";
+          this.$refs.stuForm.resetFields();
+          this.$refs.stuForm2.resetFields();
+          this.stuForm2.campusName = this.itemData.campusName;
+          this.stuForm2.classType = this.itemData.className;
+          this.stuForm.classBatch = this.itemData.classBatch;
+          this.$refs.stuForm3.resetFields();
+          this.$refs.stuForm4.resetFields();
+        });
     },
     handleCheckInfo() {
-      this.$refs.stuForm.validateField("idCardAndPhone", (errMsg) => {
+      this.$refs.stuForm.validateField("cardId", (errMsg) => {
         if (errMsg) {
           console.log("校验失败");
         } else {
           //校验通过
           this.$fetch("studentcampus_basisIdCardAndPhoneGetInfo", {
-            idCardAndPhone: this.stuForm.idCardAndPhone,
+            idCardAndPhone: this.stuForm.cardId,
           }).then((res) => {
             //根据身份证或手机查询学员信息
             if (res.data.studentCampusModel) {
               //老学员
-              for (var key in res.data.studentCampusModel) {
-                if (this.stuForm[key] != undefined) {
-                  this.stuForm[key] = res.data.studentCampusModel[key];
-                }
-              }
-              this.stuForm["provincinitValue"] =
-                res.data.studentCampusModel["provinc"];
-              this.stuForm["cityinitValue"] =
-                res.data.studentCampusModel["city"];
-              this.stuForm["countyinitValue"] =
-                res.data.studentCampusModel["county"];
-              console.log("countycountycounty1111", this.stuForm);
-              this.stuForm.studentType = "Old";
+              this.open(res.data.studentCampusModel);
+              // return;
+              // for (var key in res.data.studentCampusModel) {
+              //   if (this.stuForm[key] != undefined) {
+              //     this.stuForm[key] = res.data.studentCampusModel[key];
+              //   }
+              // }
+              // this.stuForm["provincinitValue"] =
+              //   res.data.studentCampusModel["provinc"];
+              // this.stuForm["cityinitValue"] =
+              //   res.data.studentCampusModel["city"];
+              // this.stuForm["countyinitValue"] =
+              //   res.data.studentCampusModel["county"];
+              // console.log("countycountycounty1111", this.stuForm);
+              // this.stuForm.studentType = "Old";
             } else {
               //新学员
               this.stuForm.studentType = "New";
-              this.$refs.stuForm.resetFields();
-              this.$refs.stuForm2.resetFields();
-              this.stuForm2.campusName = this.itemData.campusName;
-              this.stuForm2.classType = this.itemData.className;
-              this.$refs.stuForm3.resetFields();
-              this.$refs.stuForm4.resetFields();
+              // this.$refs.stuForm.resetFields();
+              // this.$refs.stuForm2.resetFields();
+              // this.stuForm2.campusName = this.itemData.campusName;
+              // this.stuForm2.classType = this.itemData.className;
+              // this.stuForm.classBatch = this.itemData.classBatch;
+              // this.$refs.stuForm3.resetFields();
+              // this.$refs.stuForm4.resetFields();
             }
           });
         }
@@ -1251,6 +1326,13 @@ export default {
 
       if (!this.stuForm.studentType) {
         this.$message.warning("请先选择学员类型");
+        // this.$refs.stuForm2.clearValidate("courses"); 
+        this.stuForm2.courses = [];
+        return;
+      }
+      if (!this.stuForm.saleSource) {
+        this.$message.warning("请先选择来源类型");
+        // this.$refs.stuForm2.clearValidate("courses");
         this.stuForm2.courses = [];
         return;
       }
@@ -1509,6 +1591,7 @@ export default {
           this.$refs.stuForm2.resetFields();
           this.stuForm2.campusName = this.itemData.campusName;
           this.stuForm2.classType = this.itemData.className;
+          this.stuForm.classBatch = this.itemData.classBatch;
           this.$refs.stuForm3.resetFields();
           this.$refs.stuForm4.resetFields();
           this.getTableData();
@@ -1604,7 +1687,32 @@ export default {
   },
 };
 </script>
+<style  lang="scss" >
+.elmgs_confirm-box {
+  width: 500px;
+  padding: 10px 0;
+  .el-message-box__btns {
+    width: 40%;
+    margin: 0 auto 10px;
+    .confirmButtonClass {
+      float: right !important;
+    }
+    .confirmButtonClass {
+      float: left !important;
+    }
+  }
+  .el-message-box__title {
+    text-align: center;
+    & > span {
+      display: inline-block;
+    }
+  }
 
+  .el-message-box__headerbtn {
+    font-size: 24px !important;
+  }
+}
+</style>
 <style lang="scss" scoped>
 .goods-list {
   .empty_box {
