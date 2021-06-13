f15713a4612488b9790bcc137bb1d073ca88d35c
Antony-Juicy <gabbiyabbiy9@gmail.com>
2021-06-13 14:07:43 +0800

    提交

---
+++ src/views/crm-module/weapp/draw/index.vue
@@ -0,0 +1,342 @@
+<template>
+  <div class="view-detail">
+      <search-form
+      :formOptions="formOptions"
+      :showNum="7"
+      @onSearch="onSearch"
+    ></search-form>
+    <div class="w-container">
+      <div class="btn-wrapper">
+        <el-button type="primary" size="small" @click="handleAdd"
+          >添加</el-button
+        >
+      </div>
+      <rd-table
+        :tableData="tableData"
+        :tableKey="tableKey"
+        :pageConfig.sync="pageConfig"
+        fixedTwoRow
+        @pageChange="pageChange"
+        :emptyText="emptyText"
+      >
+        <template slot="edit" slot-scope="scope">
+          <el-button @click="handleEdit(scope.row)" type="text" size="small"
+            >编辑</el-button
+          >
+          <el-divider direction="vertical"></el-divider>
+          <el-button
+            @click="handleDelete(scope.row)"
+            type="text"
+            size="small"
+            style="color: #ec5b56"
+            >删除</el-button
+          >
+        </template>
+      </rd-table>
+    </div>
+    
+    <!-- 添加 -->
+    <rd-dialog
+        :title="addStatus?'添加':'编辑'"
+        :dialogVisible="addVisible"
+        @handleClose="addVisible = false"
+        @submitForm="submitAddForm('dataForm3')"
+      >
+        <RdForm :formOptions="addFormOptions" formLabelWidth="120px" :rules="addRules" ref="dataForm3">
+          <template slot="post">
+            <el-button size="small" type="primary">上传</el-button>
+          </template>
+        </RdForm>
+      </rd-dialog>
+
+      <full-dialog
+        v-model="addVisible"
+        :title="addStatus?'添加':'编辑'"
+        @change="addVisible = false"
+      >
+        <RdForm :formOptions="addFormOptions" formLabelWidth="120px" :rules="addRules" ref="dataForm3">
+          <template slot="post">
+            <el-button size="small" type="primary">上传</el-button>
+          </template>
+        </RdForm>
+      </full-dialog>
+  </div>
+</template>
+
+<script>
+import RdForm from "@/components/RdForm";
+export default {
+  name:"view-detail",
+  data(){
+    return {
+      formOptions: [
+        {
+          prop: "menuName",
+          element: "el-input",
+          placeholder: "商品名称",
+        },
+        {
+          prop: "menuName",
+          element: "el-input",
+          placeholder: "活动名称",
+        },
+        {
+          prop: "menuName",
+          element: "el-input",
+          placeholder: "名称",
+        }
+      ],
+      searchForm:{},
+      emptyText:"暂无数据",
+      tableData:[
+         {
+          id: 1,
+          name: "飞翔的荷兰人3",
+          cutdown: 1608897351706,
+          visit: 2,
+          phone: "15692026183",
+        },
+      ],
+      tableKey: [
+        {
+          name: "ID主键",
+          value: "id",
+          fixed: "left",
+          width: 80
+        },
+        {
+          name: "老师名称",
+          value: "staffName",
+        },
+        {
+          name: "商品名称",
+          value: "goodsName",
+        },
+        {
+          name: "活动名称",
+          value: "activityName",
+        },
+        {
+          name: "名称",
+          value: "posterName",
+        },
+        {
+          name: "图片",
+          value: "posterPic",
+        },
+        {
+          name: "分享文案一",
+          value: "posterCopyFirst",
+        },
+        {
+          name: "分享文案二",
+          value: "posterCopySecond",
+        },
+        {
+          name: "分享文案三",
+          value: "posterCopyThird",
+        },
+        {
+          name: "分享文案四",
+          value: "posterCopyFourth",
+        },
+        {
+          name: "分享文案五",
+          value: "posterCopyFifth",
+        },
+        {
+          name: "创建时间",
+          value: "createAt",
+        },
+        {
+          name: "修改时间",
+          value: "updateAt",
+        },
+        {
+          name: "操作",
+          value: "edit",
+          operate: true,
+          width: 140,
+          fixed: "right"
+        },
+      ],
+       pageConfig: {
+        totalCount: 0,
+        currentPage: 1,
+        pageSize: 10,
+      },
+      addVisible: false,
+      addFormOptions: [
+          
+        {
+          prop: "menuName",
+          element: "el-input",
+          placeholder: "请输入名称",
+          label: "名称"
+        },
+        {
+          prop: "post",
+          element: "el-input",
+          placeholder: "",
+          label: "上传",
+          operate: true,
+          initValue: 0
+        },
+        {
+          prop: "roleName",
+          element: "el-select",
+          placeholder: "请选择",
+          label: "所属九块九包邮",
+          options: [
+            {
+              label: "博士",
+              value: "0",
+            },
+            {
+              label: "硕士",
+              value: 1,
+            },
+          ],
+        },
+        {
+          prop: "roleName",
+          element: "el-select",
+          placeholder: "请选择",
+          label: "所属活动",
+          options: [
+            {
+              label: "博士",
+              value: "0",
+            },
+            {
+              label: "硕士",
+              value: 1,
+            },
+          ],
+        },
+        {
+          prop: "menuName3",
+          element: "el-input",
+          placeholder: "请输入",
+          label: "分享分案一",
+          type:"textarea",
+          rows: 2
+        },
+         {
+          prop: "menuName3",
+          element: "el-input",
+          placeholder: "请输入",
+          label: "分享分案二",
+          type:"textarea",
+          rows: 2
+        },
+         {
+          prop: "menuName3",
+          element: "el-input",
+          placeholder: "请输入",
+          label: "分享分案三",
+          type:"textarea",
+          rows: 2
+        },
+         {
+          prop: "menuName3",
+          element: "el-input",
+          placeholder: "请输入",
+          label: "分享分案四",
+          type:"textarea",
+          rows: 2
+        },
+           {
+          prop: "menuName3",
+          element: "el-input",
+          placeholder: "请输入",
+          label: "分享分案五",
+          type:"textarea",
+          rows: 2
+        }
+      ],
+      addRules:{
+        updateReason: [
+          { required: true, message: "请输入修改事由", trigger: "blur" },
+        ]
+      },
+      addStatus: true,
+      editId:""
+    }
+  },
+  components:{
+    RdForm
+  },
+   methods: {
+     onSearch(val){
+       this.searchForm = {
+        ...val
+      };
+      console.log(val,this.searchForm , 'val---')
+      this.getTableData();
+     },
+     getTableData(){
+
+     },
+     pageChange(val) {
+      console.log(val,'pagechange')
+      this.pageConfig.currentPage = val.page;
+      this.pageConfig.showCount = val.limit;
+      this.getTableData();
+    },
+    handleAdd(){
+      this.addStatus = true;
+      this.addVisible = true;
+    },
+    submitAddForm(formName){
+      this.$refs[formName].validate((valid, formData) => {
+        if(valid){
+          console.log(formData, "提交");
+        }
+          
+      });
+    },
+    handleEdit(data){
+      this.addStatus = false;
+      this.addVisible = true;
+      this.addFormOptions.forEach(item => {
+           item.initValue = data[item.prop];
+      })
+      setTimeout(() => {
+        this.$refs.dataForm3.addInitValue();
+      }, 0);
+      this.editId = data.id;
+    },
+    handleDelete(row) {
+      let info = '';
+      this.$confirm(`此操作将删除此${info}, 是否继续?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.$fetch("projectType_delete", {
+            id: row.id
+          }).then((res) => {
+            if (res) {
+              this.$message({
+                message: "删除成功",
+                type: "success",
+              });
+              setTimeout(() => {
+                this.getTableData();
+              }, 50);
+            }
+          });
+        })
+        .catch(() => {});
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.view-detail {
+
+}
+</style>
